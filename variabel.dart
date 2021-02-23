49a8a79aa2a6415f0aecfc657edd4b52cc51a411
dwpujimuliati <78520963+dwpujimuliati@users.noreply.github.com>
2021-02-23 22:49:25 +0700

    Create variabel.dart

---
+++ variabel.dart
@@ -0,0 +1,14 @@
+void main(){ 
+     // membaut variabel dengan tipe data 
+     String nama = "Dian"; 
+     int umur = 23; 
+     double tinggi = 180.43;
+     bool isMenikah = false; 
+
+     var alamat = "Lombok, Indonesia"; 
+
+     // mencetak variabel 
+     print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm."); 
+     print("Menikah: $isMenikah"); 
+     print("Alamat: $alamat");
+}
