bbc4c7c7b2d3d713d8fdc540681e6c20e3a7b563
dwpujimuliati <78520963+dwpujimuliati@users.noreply.github.com>
2021-02-23 22:59:19 +0700

    Update variabel.dart

--- variabel.dart
+++ variabel.dart
@@ -1,5 +1,4 @@
 void main(){ 
-     // membaut variabel dengan tipe data 
      String nama = "Dian"; 
      int umur = 23; 
      double tinggi = 180.43;
@@ -7,7 +6,6 @@ void main(){
 
      var alamat = "Lombok, Indonesia"; 
 
-     // mencetak variabel 
      print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm."); 
      print("Menikah: $isMenikah"); 
      print("Alamat: $alamat");
